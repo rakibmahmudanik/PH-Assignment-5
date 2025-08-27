c37f8676e21b8feb00cf9c7aa4957db3c85a5da7
RAKIB MAHMUD <77047747+rakibmahmudanik@users.noreply.github.com>
2025-08-27 14:52:15 +0600

    Added Call history funtionality with velidate

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ const cards = document.querySelectorAll(".card");
 const hearts = document.querySelectorAll(".heart");
 const heartsCount = document.getElementById("hearts-count");
 const coinsCount = document.getElementById("coins-count");
+const history = document.querySelector(".history-data");
 
 let counter = 1;
 for (const heart of hearts) {
@@ -13,12 +14,28 @@ for (const heart of hearts) {
 for (const card of cards) {
   const title = card.querySelector(".card-titles h2").innerText;
   const callNumber = card.querySelector(".card-number h1").innerText;
-  card.querySelector(".call").addEventListener("click", function () {
+  card.querySelector(".call").addEventListener("click", function (e) {
     let coins = parseInt(coinsCount.innerText);
     if (coins >= 20) {
       let remainingCoins = coins - 20;
       coinsCount.innerText = remainingCoins;
-      alert(`${title}\n${callNumber}`);
+
+      alert(`Calling ${title} ${callNumber} ...`);
+
+      if (e.target.classList.contains("call")) {
+        const now = new Date();
+        const time = now.toLocaleTimeString();
+
+        const li = document.createElement("li");
+        li.innerHTML = `<div class="info">
+                <p>${title}</p>
+                <span>${callNumber}</span>
+              </div>
+              <span class="time">${time}</span>
+            `;
+
+        history.appendChild(li);
+      }
     } else {
       alert("insufficient Coin Balance!");
     }
